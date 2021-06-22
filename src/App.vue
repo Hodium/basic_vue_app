2b8bd2e7f4107edec75bbcd0bedec61a0815d74b
Pedro Diogo Oliveira <pedro.diogo.oliveira@gmail.com>
2021-06-22 01:27:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label>THIS IS A GOOD TEST</label>
+    <label>API WORKS -> {{ apiresult }}</label>
+
+    <label>BUT THIS IS EVEN BETTER MOFO!!</label>
+    <label>{{ otherApiResult }}</label>
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      apiresult: "",
+    };
+  },
+  mounted() {
+    Axios.get("/test").then((res) => {
+      this.apiresult = res.data;
+    });
+    Axios.get("/anotherTest").then((res) => {
+      this.otherApiResult = res.data;
+    });
+  },
+};
+</script>
+
+<style>
+</style>
